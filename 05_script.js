4e97724328791afcfc1a66fe69b9e0c60d98aebb
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-10-08 22:24:39 +0800

    Update 05_script.js

--- 05_script.js
+++ 05_script.js
@@ -7,20 +7,19 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityInput.value+'&app
 .then(res=>res.json())
 .then (data => {
 
-let city = document.querySelector('#cityname')
-let temp = document.querySelector('#temp')
-let text = document.querySelector('#condition')
-let wind = document.querySelector('#wind')
+    let nameval = data['name']
+    let descrip = data['weather']['0']['description']
+    let temperature = data['main']['temp']
+    let wndspd = data['wind']['speed']
+    let city = document.querySelector('#cityname')
+    let temp = document.querySelector('#temp')
+    let text = document.querySelector('#condition')
+    let wind = document.querySelector('#wind')
 
-function convertion(val){
-    return (val - 273).toFixed(2)
-}
-
-    var nameval = data['name']
-    var descrip = data['weather']['0']['description']
-    var temperature = data['main']['temp']
-    var wndspd = data['wind']['speed']
-
+    function convertion(val) {
+        return (val-273).toFixed(1);
+        
+    }
 
     //Now with the help of innerHTML you have to make arrangements to display all the information in the webpage.
     city.innerHTML=`Weather of ${nameval}`
